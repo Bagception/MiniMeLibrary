a60c5fec6ba36072fb9439a1e616c17f24553431
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2013-10-24 16:04:40 +0200

    rework RFIDMiniMe library, hashTagList + replaced BroadcastTagFound method

--- gen/de/uniulm/bagception/service/USBConnectionServiceRemote.java
+++ gen/de/uniulm/bagception/service/USBConnectionServiceRemote.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/phil/workspace/MiniMeLibrary/src/de/uniulm/bagception/service/USBConnectionServiceRemote.aidl
+ * Original file: /home/xaffe/Repo/bagception/MiniMeLibrary/src/de/uniulm/bagception/service/USBConnectionServiceRemote.aidl
  */
 package de.uniulm.bagception.service;
 public interface USBConnectionServiceRemote extends android.os.IInterface
